c98aa8862f8a425311dde0101734f234ae8fec55
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-11-20 22:36:42 -0500

    added comments

--- server/server.py
+++ server/server.py
@@ -4,20 +4,25 @@ from openai import OpenAI
 import os
 
 app = FastAPI()
+
+# Added OPENAI to the server
 client = OpenAI()
+
+# Grab API KEY from local enviroment (need to change to env file in future!!!)
 api_key = os.environ.get('OPENAI_API_KEY')
 
 if api_key:
     print('API key found!')
-    print(api_key)
 else:
     print('Error!')
 
+
+# Server allows all URLs to access server (need to change in the future to more secure URL)
 origins = [
     "*"
     ]
 
-
+# Parameters for request to OPENAI to edit images (Need learn to add images to params for editing)
 image_params = {
     "n": 1,
     "size": "1024x1024",
@@ -25,6 +30,7 @@ image_params = {
 
 }
 
+# Middleware decloration for all headers and methods
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -34,7 +40,7 @@ app.add_middleware(
 )
 
 
-
+# Initializing the home URL (Need to learn how to get to react frontend)
 @app.get("/")
 async def root():
     return {"message": "Hello from the server!"}
