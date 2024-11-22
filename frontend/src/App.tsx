dc6c1fef8423a0e3bd6f7e5e9c4af3215c0edf19
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-11-21 22:49:43 -0500

    setting up the frontend for uploading the image.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/Button";
 
 function App() {
+  // Need to reach the backend with a function that allows me to upload a photo.
+  const handleSubmit = (): void => {
+    const fileInput = document.getElementById(
+      "imageUpload"
+    ) as HTMLInputElement | null;
+    if (fileInput?.files?.[0]) {
+      const file = fileInput.files[0];
+    }
+  };
+
+  //  Handle change of image when you upload it to the frontend
+  interface FileInputEvent extends React.ChangeEvent<HTMLInputElement> {}
+
+  const handleChange = (e: FileInputEvent): void => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  // State for files that get added  to the frontend
+  const [file, setFile] = useState<File | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        id="imageUpload"
+        onChange={handleChange}
+      />
+      {/* File added only if there is a file to be displayed from the state */}
+      {file && (
+        <img src={URL.createObjectURL(file)} alt={file.name} width={300} />
+      )}
+      <Button onClick={handleSubmit}>Upload Image</Button>
+
+      <Button onClick={() => {}}>Edit Image</Button>
     </div>
   );
 }
